28ad7d3097956f9fbaf054adef6a0e3f37f2d8ad
danielchan-25 <alexchan0325@gmail.com>
2024-12-12 21:25:18 +0800

    Update

--- 20240619_双色球/spider.py
+++ 20240619_双色球/spider.py
@@ -3,10 +3,13 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
 # 日期：2024年7月4日
 # 功能：双色球数据爬取
 # 定时：23:50
+
+file_name = '双色球数据.csv'
+columns = ['期号', '号码1', '号码2', '号码3', '号码4', '号码5', '号码6', '号码7', '快乐星期天', '奖池奖金(元)',
+           '一等奖注数', '一等奖奖金(元)', '二等奖注数', '二等奖奖金(元)', '总投注额(元)', '开奖日期']
 
 
 def get_data():
@@ -26,16 +29,15 @@ def get_history_data(rows):
         cells = i.find_all('td')
         result = [cell.get_text(strip=True) for cell in cells]
         data.append(result)
+    df = pd.DataFrame(data, columns=columns)
+    df = df.sort_values(by='开奖日期')
+    return df
 
-    columns = ['期号', '号码1', '号码2', '号码3', '号码4', '号码5', '号码6', '号码7', '快乐星期天', '奖池奖金(元)',
-               '一等奖注数', '一等奖奖金(元)', '二等奖注数', '二等奖奖金(元)', '总投注额(元)', '开奖日期']
-    return pd.DataFrame(data, columns=columns)
 
-
-def get_today_data(rows):
+def get_today_data(rows) -> pd.DataFrame:
     ''' 获取当日数据 '''
     data = [i.text for i in rows[0]][1:]
-    return pd.DataFrame([data])
+    return pd.DataFrame([data], columns=columns)
 
 
 url = 'https://datachart.500.com/ssq/history/newinc/history.php'
@@ -47,8 +49,11 @@ headers = {
 if __name__ == '__main__':
     # 初次运行时使用，获取历史数据
     # df = get_history_data(get_data())
-    # df.to_csv('双色球数据.csv', mode='w', index=False)
+    # df.to_csv(file_name, mode='w', index=False)
 
     # 每日更新
-    df = get_today_data(get_data())
-    df.to_csv('双色球数据.csv', mode='a', index=False, header=False)
+    row = get_today_data(get_data())
+    if not row.empty:
+        df = pd.read_csv(file_name)
+        if row['开奖日期'].iloc[-1] != df['开奖日期'].iloc[-1]:
+            row.to_csv(file_name, mode='a', index=False, header=False)
